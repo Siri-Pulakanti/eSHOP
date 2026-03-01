957347e1a40a3f530fd111b20583c2b8ff769eb4
Siri-Pulakanti <pulakantisiri1@gmail.com>
2026-03-01 16:18:48 +0530

    price Updation

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,8 +1,10 @@
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
 import "./Subtotal.css";
+import { getBasketTotal } from "./reducer";
 
 export default function Subtotal() {
-  //   const [{ basket }, dispatch] = useStateValue();
+  const [{ basket }, dispatch] = useStateValue();
 
   return (
     <div className="subtotal">
@@ -10,8 +12,8 @@ export default function Subtotal() {
         renderText={(value) => (
           <>
             <p>
-              {/* Subtotal ({basket.length} items):<strong>${value}</strong> */}
-              Subtotal (0 items):<strong>$0</strong>
+              Subtotal ({basket.length} items):<strong>${value}</strong>
+              {/* Subtotal (0 items):<strong>$0</strong> */}
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -19,8 +21,7 @@ export default function Subtotal() {
           </>
         )}
         decimalScale={2}
-        // value={getBasketTotal(basket)}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
       />
--- src/reducer.js
+++ src/reducer.js
@@ -2,9 +2,9 @@ export const initialState = {
   basket: [],
 };
 
-// export const getBasketTotal = (basket) => {
-//   return basket?.reduce((amount, item) => item.price + amount, 0);
-// };
+export const getBasketTotal = (basket) => {
+  return basket?.reduce((amount, item) => item.price + amount, 0);
+};
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_BASKET":
